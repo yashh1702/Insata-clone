c367638f80c96fab5f09358d93b2e03c02292bcf
yashh1702 <yagrawal944@gmail.com>
2026-04-20 19:24:50 +0530

    Refactor Login component to improve structure and loading state handling

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,40 +1,49 @@
 import "../styles/form.scss"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
+import { useAuth } from '../hooks/useAuth.js'
 
 const Login = () => {
-      const [ username, setUsername ] = useState("")
-    const [ password, setPassword ] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+    const { handleLogin , loading } = useAuth()
 
+    if (loading) {
+        return (<main>
+            <h1>Loading.....</h1>
+        </main>)
+    }
 
     function handleSubmit(e) {
         e.preventDefault()
 
-       
+        handleLogin(username, password).then(res => console.log(res))
+        navigate("/")
     }
 
-  return (
-    <main>
-        <div className="form-container">
-            <h1>Login Form</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                value={username}
-                onInput={(e) => { setUsername(e.target.value) }}
-                type='text'
-                placeholder='Enter Your username'/> 
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login Form</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        value={username}
+                        onInput={(e) => { setUsername(e.target.value) }}
+                        type='text'
+                        placeholder='Enter Your username' />
 
-                <input 
-                value={password}
-                onInput={(e)=>{setPassword(e.target.value)}}
-                type='text'
-                placeholder='Enter Your Password'/> 
-                <button type='submit'>Login</button>
-            </form>
-            <p>Don't have an account? <Link className='toggleAuthForm' to="/register">Login</Link></p>
-        </div>
-    </main>
-  )
+                    <input
+                        value={password}
+                        onInput={(e) => { setPassword(e.target.value) }}
+                        type='text'
+                        placeholder='Enter Your Password' />
+                    <button type='submit'>Login</button>
+                </form>
+                <p>Don't have an account? <Link className='toggleAuthForm' to="/register">Login</Link></p>
+            </div>
+        </main>
+    )
 }
 
 export default Login
