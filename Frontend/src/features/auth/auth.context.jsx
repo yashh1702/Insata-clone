656d55e1dd9178e0b40c3b026b716909331e18a0
yashh1702 <yagrawal944@gmail.com>
2026-04-22 15:38:34 +0530

    Refactor Auth context and useAuth hook to streamline user authentication handling

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState } from 'react'
-import { login, register, getMe } from './services/auth.api'
 
 export const AuthContext = createContext()
 
@@ -7,32 +6,9 @@ export function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(false)
 
-    const handleLogin = async (username, password) => {
-        setLoading(true)
-        try {
-            const response = await login(username, password)
-            setUser(response.user)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    const handleRegister = async (username, email, password) => {
-        setLoading(true)
-        try {
-            const response = await register(username, email, password)
-            setUser(response.user)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
-        }
-    }
 
     return(
-        <AuthContext.Provider value={{user,loading,handleLogin,handleRegister}}>
+        <AuthContext.Provider value={{user,loading,setUser,setLoading}}>
             {children}
         </AuthContext.Provider>
     )
--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,8 +1,37 @@
 import { useContext } from 'react'
-import {AuthContext} from '../auth.context.jsx'
+import { AuthContext } from '../auth.context.jsx'
+import { register, login } from '../services/auth.api.js'
 
-export function useAuth(){
+export function useAuth() {
     const context = useContext(AuthContext)
+    const { user, loading, setUser, setLoading } = context
 
-    return context
+    const handleLogin = async (username, password) => {
+        setLoading(true)
+        try {
+            const response = await login(username, password)
+            setUser(response.user)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleRegister = async (username, email, password) => {
+        setLoading(true)
+        try {
+            const response = await register(username, email, password)
+            setUser(response.user)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return {
+        user, loading, handleLogin, handleRegister
+    }
+
 }
