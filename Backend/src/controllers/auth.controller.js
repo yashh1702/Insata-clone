a01faaffb6ee3284472af0b653ba8ada52a8e17d
yashh1702 <yagrawal944@gmail.com>
2026-04-23 15:57:10 +0530

    Add password selection to user model and login controller for authentication

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -71,7 +71,7 @@ async function loginController (req, res) {
                 email: email
             }
         ]
-    })
+    }).select("+password")
 
     if (!user) {
         return res.status(404).json({
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -13,7 +13,8 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, "password is required"]
+        required: [true, "password is required"],
+        select:false
     },
     bio:String,
     profileImage:{
